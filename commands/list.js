53d0fe3e67ab52eaa3b5292eb1a670584c09226f
Daniel Raine <draine@modernatx.com>
2021-12-02 18:46:59 -0500

    create CLI utility

---
+++ commands/list.js
@@ -0,0 +1,5 @@
+const list = () => {
+    console.log('You do not have any tasks yet');
+}
+
+module.exports = list;
---
+++ index.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+const { program } = require('commander')
+const list = require('./commands/list')
+
+program
+    .command('list')
+    .description('List tasks')
+    .action(list)
+
+program.parse();
