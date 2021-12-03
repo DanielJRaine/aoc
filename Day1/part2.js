457c4d57f1d9a55325bbcb0cd957405b5fb4ad38
Daniel Raine <draine@modernatx.com>
2021-12-02 23:09:21 -0500

    group sliding windows

--- Day1/part2.js
+++ Day1/part2.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+const fs = require('fs')
+const util = require('util')
 
 let depths = fs.readFileSync('./depths.txt', {
     encoding: "utf8",
@@ -9,5 +10,15 @@ depths = depths.split('\n').map(Number)
 
 let depthIncreasedCount = 0;
 let previousDepth;
+const windowLength = 3;
+const windows = [];
 
-// todo: 
+// todo: group depths into sets of 3
+console.log(depths);
+
+for (let i=0; i<depths.length - windowLength; i++) {
+    windows[i] = [depths[i], depths[i+1], depths[i+2]] 
+}
+
+// process.stdout.write(windows);
+console.log(util.inspect(windows, { maxArrayLength: null }))
