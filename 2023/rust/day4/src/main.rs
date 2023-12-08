6c079a69348145be946166424312b2b5d5a2c982
Daniel Raine <daniel.raine@modernatx.com>
2023-12-08 13:43:55 -0500

    recursively collect winning cards

--- 2023/rust/day4/src/main.rs
+++ 2023/rust/day4/src/main.rs
@@ -1,6 +1,7 @@
 use std::{env};
 use std::collections::HashMap;
 use std::fs::read_to_string;
+use std::ops::Deref;
 use eyre::{bail, eyre};
 use jane_eyre::Result;
 use regex::{Regex};
@@ -24,11 +25,77 @@ fn main() -> Result<()> {
 
 fn part1() -> Result<()> {
     let input: String = aoc::read_input();
-    let mut acc = 0;
+    let mut acc: u32 = 0;
     
     for line in input.lines() {
         // take_until: ? while is_numeric
         let (header, num_str) = line.split_once(':').unwrap();
+        let (winning_strs, strs_you_have) = num_str.split_once('|').unwrap();
+        
+        // parse into i32
+        let winning_nums: Vec<i32> = winning_strs
+            .trim()
+            .split_whitespace()
+            .map(|n| n.parse::<i32>().unwrap())
+            .collect();
+        
+        let mut nums_you_have: Vec<i32> = strs_you_have
+            .trim()
+            .split_whitespace()
+            .map(|n| n.parse::<i32>().unwrap())
+            .collect();
+            // .chars();
+        
+        nums_you_have.sort();
+        
+        let mut score = 1u32;
+        for num in nums_you_have {
+            if winning_nums.contains(&num) {
+                score <<= 1;
+            }
+        }
+        
+        // account for initial value
+        println!("score: {score}");
+        score >>= 1;
+        acc += score;
+        // "Card   1: 33 34 29 52 91  7 31 42  2  6 | 53 52  6 96 42 91  2 23  7 38 90 28 31 51  1 26 33 22 95 34 29 77 32 86  3"
+    }
+    
+    println!("sum: {acc}");
+    Ok(())
+}
+
+fn check_for_winning_nums(winning_nums: &Vec<i32>, nums_you_have: &Vec<i32>) -> usize {
+    let mut score = 1;
+    
+    for num in nums_you_have {
+        if winning_nums.contains(&num) {
+            score += 1;
+        }
+    }
+    
+    score
+}
+
+#[derive(Debug, Clone)]
+struct Card {
+    id: usize,
+    winning_nums: Vec<i32>,
+    nums_you_have: Vec<i32>,
+}
+
+fn part2() -> Result<()> {
+    let input: String = aoc::read_input();
+    let mut cards: Vec<Card> = vec![];
+    let mut won_cards: Vec<&Card> = vec![];
+    let mut acc: u32 = 0;
+    
+    for (id, line) in input.lines().enumerate() {
+        // take_until: ? while is_numeric
+        
+        let (header, num_str) = line.split_once(':').unwrap();
+        
         let (winning_strs, strs_you_have) = num_str.split_once('|').unwrap();
         
         // parse into i32
@@ -43,24 +110,30 @@ fn part1() -> Result<()> {
             .split_whitespace()
             .map(|n| n.parse::<i32>().unwrap())
             .collect();
-            // .chars();
         
-        
-        //
-        // "Card   1: 33 34 29 52 91  7 31 42  2  6 | 53 52  6 96 42 91  2 23  7 38 90 28 31 51  1 26 33 22 95 34 29 77 32 86  3"
-        
-        dbg!()
+        cards.push(Card {
+            id,
+            winning_nums,
+            nums_you_have,
+        })
     }
     
-    dbg!();
+    for card in cards.clone() {
+        collect_winning_cards(&card, &cards, &mut won_cards);
+    }
+    
+    // println!("sum: {acc}");
     Ok(())
 }
 
-fn part2() -> Result<()> {
-    todo!();
+fn collect_winning_cards<'a>(card: &Card, cards: &'a Vec<Card>, won_cards: &mut Vec<&'a Card>) -> Vec<Card> {
+    let card_score = check_for_winning_nums(&card.winning_nums, &card.nums_you_have);
+    for c in cards.iter().skip(card.id).take(card_score).collect::<Vec<&Card>>() {
+        
+        won_cards.push(c);
+    }
     
-    dbg!();
-    Ok(())
+    vec![]
 }
 
 #[cfg(test)]
