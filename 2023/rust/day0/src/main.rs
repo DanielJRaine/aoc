ebd41dad134701eadcf0a83255a4b3e2ea12de44
Daniel Raine <daniel.raine@modernatx.com>
2023-12-08 08:45:46 -0500

    parse into nums

--- 2023/rust/day0/src/main.rs
+++ 2023/rust/day0/src/main.rs
@@ -11,14 +11,14 @@ fn main() -> Result<()> {
     
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        bail!("Must provide part #. Allowed values: {:?}", vec![1,2]);
+        color_eyre::eyre::bail!("Must provide part #. Allowed values: {:?}", vec![1,2]);
     }
     
     let part_number = &args[1];
     match part_number.as_str() {
         "1" => part1(),
         "2" => part2(),
-        _ => Err(eyre!("Select part 1 or 2")),
+        _ => Err(color_eyre::eyre::eyre!("Select part 1 or 2")),
     }
 }
 
@@ -28,13 +28,14 @@ fn part1() -> Result<()> {
     
     }
     
-    println!("");
+    dbg!();
     Ok(())
 }
 
 fn part2() -> Result<()> {
     todo!();
     
+    dbg!();
     Ok(())
 }
 
@@ -42,8 +43,8 @@ fn part2() -> Result<()> {
 mod tests {
     use super::*;
     
-    // #[test]
-    // fn it_parses() {
-    //     assert_eq!(18, parse_alphanumeric("oneight"))
-    // }
+    #[test]
+    fn it_eq() {
+        assert_eq!(1, 1);
+    }
 }
--- 2023/rust/day4/src/main.rs
+++ 2023/rust/day4/src/main.rs
@@ -1,3 +1,74 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env};
+use std::collections::HashMap;
+use std::fs::read_to_string;
+use eyre::{bail, eyre};
+use jane_eyre::Result;
+use regex::{Regex};
+use aoc;
+
+fn main() -> Result<()> {
+    jane_eyre::install()?;
+    
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        color_eyre::eyre::bail!("Must provide part #. Allowed values: {:?}", vec![1,2]);
+    }
+    
+    let part_number = &args[1];
+    match part_number.as_str() {
+        "1" => part1(),
+        "2" => part2(),
+        _ => Err(color_eyre::eyre::eyre!("Select part 1 or 2")),
+    }
+}
+
+fn part1() -> Result<()> {
+    let input: String = aoc::read_input();
+    let mut acc = 0;
+    
+    for line in input.lines() {
+        // take_until: ? while is_numeric
+        let (header, num_str) = line.split_once(':').unwrap();
+        let (winning_strs, strs_you_have) = num_str.split_once('|').unwrap();
+        
+        // parse into i32
+        let winning_nums: Vec<i32> = winning_strs
+            .trim()
+            .split_whitespace()
+            .map(|n| n.parse::<i32>().unwrap())
+            .collect();
+        
+        let nums_you_have: Vec<i32> = strs_you_have
+            .trim()
+            .split_whitespace()
+            .map(|n| n.parse::<i32>().unwrap())
+            .collect();
+            // .chars();
+        
+        
+        //
+        // "Card   1: 33 34 29 52 91  7 31 42  2  6 | 53 52  6 96 42 91  2 23  7 38 90 28 31 51  1 26 33 22 95 34 29 77 32 86  3"
+        
+        dbg!()
+    }
+    
+    dbg!();
+    Ok(())
+}
+
+fn part2() -> Result<()> {
+    todo!();
+    
+    dbg!();
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn it_eq() {
+        assert_eq!(1, 1)
+    }
 }
