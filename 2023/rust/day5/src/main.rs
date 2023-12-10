ce6e7aef5a39e65fb7ed4bf0911c337f7dfabd3e
Daniel Raine <daniel.raine@modernatx.com>
2023-12-10 13:08:59 -0500

    parse ranges

--- 2023/rust/day5/src/main.rs
+++ 2023/rust/day5/src/main.rs
@@ -23,30 +23,46 @@ fn main() -> Result<()> {
 	}
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct ResourceMap {
 	ranges: Vec<ResourceRange>
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct ResourceRange {
-	source_range_start: usize,
 	destination_range_start: usize,
+	source_range_start: usize,
 	range_length: usize
 }
 
 impl ResourceRange {
 	fn to(&self, resource: usize) -> usize {
 		// checks source range, returns correct destination resource
-		if (self.source_range_start..self.range_length).contains(&resource) {
+		if (self.source_range_start..self.source_range_start+self.range_length).contains(&resource) {
 			// calculate the offset from the range start
 			let offset = resource - self.source_range_start;
+			
 			// take the same offset and apply it to the destination range to get the returned resource
 			self.destination_range_start + offset
 		} else {
 			resource
 		}
 	}
+}
+
+fn parse_ranges(input: &str) -> Vec<ResourceRange> {
+	let mut ranges = vec![];
+	for line in input.trim().lines() {
+		let mut iter = line.split_ascii_whitespace()
+			.map(|num| num.parse::<usize>().unwrap());
+		ranges.push(ResourceRange {
+			destination_range_start: iter.next().to_owned().unwrap(),
+			source_range_start: iter.next().to_owned().unwrap(),
+			range_length: iter.next().to_owned().unwrap(),
+		});
+	}
+	
+	ranges
 }
 
 fn part1() -> Result<()> {
@@ -56,8 +72,15 @@ fn part1() -> Result<()> {
 	// split on ':'
 	
 	let seeds_input = "79 14 55 13";
+	// let seeds = [];
+	
 	let seed_to_soil_input = "50 98 2
 		52 50 48";
+	
+	let seed_to_soil_map = ResourceMap {
+		ranges: parse_ranges(seed_to_soil_input)
+	};
+	
 	
 	let soil_to_fertilizer_input = "0 15 37
 		37 52 2
@@ -108,11 +131,39 @@ mod tests {
 	}
 	
 	#[test]
+	fn parses_ranges() {
+		let range = "
+		0 15 37
+		37 52 2
+		";
+		
+		let solution = vec![
+			ResourceRange {
+				destination_range_start: 0,
+				source_range_start: 15,
+				range_length: 37,
+			},
+			ResourceRange {
+				destination_range_start: 37,
+				source_range_start: 52,
+				range_length: 2,
+			},
+		];
+		
+		let attempt = parse_ranges(range);
+		debug_assert_eq!(attempt, solution)
+	}
+	
+	#[test]
 	fn finds_destination() {
 		let res_range = ResourceRange {
-			source_range_start: 50,
-			destination_range_start: 98,
+			destination_range_start: 50,
+			source_range_start: 98,
 			range_length: 2,
 		};
+		
+		let destination_resource = 51;
+		let source_resource = 99;
+		assert_eq!(res_range.to(source_resource), destination_resource)
 	}
 }
